9436c301eeb2a7ae54fd65f957414ad952e0efc1
pyleaf <zhiwei_zh@126.com>
2021-08-19 10:06:44 +0800

    Add files via upload

---
+++ data/first-round-wu.js
@@ -0,0 +1,152 @@
+var wuRoundData = [
+  {
+    "answer": "A", 
+    "question": "\u4e2d\u5c71\u5927\u5b66\u5357\u6821\u533a\u5185\u7684\u9648\u5bc5\u606a\u6545\u5c45\u53c8\u540d?", 
+    "options": [
+      "A\u3001\u9ebb\u91d1\u58a8\u5c4b\u4e00\u53f7", 
+      "B\u3001\u9ebb\u91d1\u58a8\u5c4b\u4e8c\u53f7", 
+      "C\u3001\u9ed1\u77f3\u5c4b", 
+      "D\u3001\u5341\u53cb\u5802"
+    ]
+  }, 
+  {
+    "answer": "A", 
+    "question": "\u4e2d\u5c71\u5927\u5b66\u76ee\u524d\u6709\u51e0\u4e2a\u6821\u533a?", 
+    "options": [
+      "A\u3001\u4e09\u4e2a", 
+      "B\u3001\u56db\u4e2a", 
+      "C\u3001\u4e94\u4e2a", 
+      "D\u3001 \u516d\u4e2a"
+    ]
+  }, 
+  {
+    "answer": "B", 
+    "question": "\u5b59\u4e2d\u5c71\u94dc\u50cf\u7684\u6350\u8d60\u8005\u662f?", 
+    "options": [
+      "A\u3001 \u9648\u5609\u5e9a", 
+      "B\u3001\u6885\u5c4b\u5e84\u5409", 
+      "C\u3001\u5b89\u5e03\u96f7\u00b7\u53f2\u6000\u58eb", 
+      "D\u3001\u9ebb\u91d1\u58a8\u592b\u4eba"
+    ]
+  }, 
+  {
+    "answer": "D", 
+    "question": "\u83b7\u8d60\u7684\u5b59\u4e2d\u5c71\u94dc\u50cf\u4e00\u5171\u6709\u51e0\u5c0a?", 
+    "options": [
+      "A\u3001\u4e00\u5c0a", 
+      "B\u3001\u4e24\u5c0a", 
+      "C\u3001\u4e09\u5c0a", 
+      "D\u3001\u56db\u5c0a"
+    ]
+  }, 
+  {
+    "answer": "D", 
+    "question": "\u5b59\u4e2d\u5c71\u5148\u751f\u300a\u975e\u5b66\u95ee\u65e0\u4ee5\u5efa\u8bbe\u300b\u7684\u6f14\u8bb2\u5730\u662f\uff1f", 
+    "options": [
+      "A\u3001\u683c\u5170\u5802", 
+      "B\u3001\u5341\u53cb\u5802", 
+      "C\u3001\u6000\u58eb\u5802", 
+      "D\u3001\u9a6c\u4e01\u5802"
+    ]
+  }, 
+  {
+    "answer": "A", 
+    "question": "\u4e0e\u5cad\u5357\uff08\u5927\u5b66\uff09\u5b66\u9662MBA\u4e2d\u5fc3\u5927\u697c\u76f8\u90bb\u7684\u5efa\u7b51\u4e3a\uff1f", 
+    "options": [
+      "A\u3001\u53f6\u8446\u5b9a\u5802", 
+      "B\u3001\u9ec4\u4f20\u7ecf\u5802", 
+      "C\u3001\u4f0d\u821c\u5fb7\u56fe\u4e66\u9986", 
+      "D\u3001\u5cad\u5357\u5802"
+    ]
+  }, 
+  {
+    "answer": "C", 
+    "question": "\u5cad\u5357MBA\u4e2d\u5fc3\u5927\u697c\u843d\u6210\u4f7f\u7528\u65f6\u5728\u54ea\u4e00\u5e74", 
+    "options": [
+      "A\u30012007", 
+      "B\u30012008", 
+      "C\u30012009", 
+      "D\u30012010"
+    ]
+  }, 
+  {
+    "answer": "C", 
+    "question": "\u5eb7\u4e50\u56ed\u5185\u7b2c\u4e00\u680b\u6c38\u4e45\u6027\u5efa\u7b51\u662f?", 
+    "options": [
+      "A\u3001\u6000\u58eb\u5802", 
+      "B\u3001\u683c\u5170\u5802", 
+      "C\u3001\u9a6c\u4e01\u5802", 
+      "D\u3001\u5341\u53cb\u5802"
+    ]
+  }, 
+  {
+    "answer": "D", 
+    "question": "\u4ee5\u4e0b\u54ea\u4e2a\u5efa\u7b51\u4e0d\u662f\u4f4d\u4e8e\u4e2d\u5927\u5357\u6821\u56ed\u4e2d\u8f74\u7ebf\u4e0a\uff1f", 
+    "options": [
+      "A\u3001\u6000\u58eb\u5802", 
+      "B\u3001\u5cad\u5357\u5802", 
+      "C\u3001\u60fa\u4ead", 
+      "D\u3001\u9a6c\u4e01\u5802"
+    ]
+  }, 
+  {
+    "answer": "B", 
+    "question": "\u9a6c\u4e01\u5802\u73b0\u4e3a\u54ea\u4e2a\u9662\u7cfb\u7684\u6240\u4f7f\u7528\uff1f", 
+    "options": [
+      "A\u3001\u5386\u53f2\u5b66\u7cfb", 
+      "B\u3001\u4eba\u7c7b\u5b66\u7cfb", 
+      "C\u3001\u54f2\u5b66\u7cfb", 
+      "D\u3001\u6cd5\u5b66\u7cfb"
+    ]
+  }, 
+  {
+    "answer": "C", 
+    "question": "\u60fa\u4ead\u4e3a\u54ea\u4e00\u5e74\u6bd5\u4e1a\u751f\u6240\u6350\u5efa\uff1f", 
+    "options": [
+      "A\u30011924", 
+      "B\u30011926", 
+      "C\u30011928", 
+      "D\u30011932"
+    ]
+  }, 
+  {
+    "answer": "B", 
+    "question": "\u4ee5\u4e0b\u54ea\u4e2a\u4e0d\u5c5e\u4e8e\u4e2d\u5927\u5317\u95e8\u724c\u574a\u7684\u9898\u5b57\uff1f", 
+    "options": [
+      "A\u3001\u683c\u81f4", 
+      "B\u3001\u8bda\u4fe1", 
+      "C\u3001\u4fee\u9f50", 
+      "D\u3001\u6cbb\u5e73"
+    ]
+  }, 
+  {
+    "answer": "D", 
+    "question": "\u5357\u6821\u56ed\u5185\uff0c\u4ee5\u6148\u5584\u5bb6\u90b5\u9038\u592b\u5148\u751f\u547d\u540d\u7684\u201c\u9038\u592b\u697c\u201d\u662f", 
+    "options": [
+      "A\u3001\u5b9e\u9a8c\u697c", 
+      "B\u3001\u5b66\u751f\u6d3b\u52a8\u4e2d\u5fc3", 
+      "C\u3001\u884c\u653f\u697c", 
+      "D\u3001\u6559\u5b66\u697c"
+    ]
+  }, 
+  {
+    "answer": "A", 
+    "question": "\u5357\u6821\u56ed\u5185\uff0c\u4ee5\u6148\u5584\u5bb6\u6881\u92b6\u741a\u5148\u751f\u547d\u540d\u7684\u201c\u6881\u92b6\u741a\u5802\u201d\u662f", 
+    "options": [
+      "A\u3001\u793c\u5802", 
+      "B\u3001\u884c\u653f\u6b27", 
+      "C\u3001\u6559\u5b66\u697c", 
+      "D\u3001\u6559\u5b66\u697c"
+    ]
+  }, 
+  {
+    "answer": "C", 
+    "question": "\u5357\u6821\u56ed\u5185\uff0c\u4ee5\u7231\u56fd\u4fa8\u5546\u718a\u5fb7\u9f99\u5148\u751f\u547d\u540d\u7684\u5efa\u7b51\u662f", 
+    "options": [
+      "A\u3001\u5b9e\u9a8c\u697c", 
+      "B\u3001\u884c\u653f\u697c", 
+      "C\u3001\u5b66\u751f\u6d3b\u52a8\u4e2d\u5fc3", 
+      "D\u3001\u6559\u5b66\u697c"
+    ]
+  }
+]
---
+++ data/third-round.js
@@ -0,0 +1,596 @@
+var thirdRoundData = [
+  {
+    "label": "\u5b59\u4e2d\u5c71"
+  }, 
+  {
+    "label": "\u6000\u58eb\u5802"
+  }, 
+  {
+    "label": "\u535a\u5b66"
+  }, 
+  {
+    "label": "\u5de5\u5546\u7ba1\u7406"
+  }, 
+  {
+    "label": "\u725b\u592b\u4eba"
+  }, 
+  {
+    "label": "\u90b9\u9c81"
+  }, 
+  {
+    "label": "\u9a6c\u4e01\u5802"
+  }, 
+  {
+    "label": "\u5ba1\u95ee"
+  }, 
+  {
+    "label": "\u7ecf\u6d4e\u5b66"
+  }, 
+  {
+    "label": "\u5c0f\u751c\u751c"
+  }, 
+  {
+    "label": "\u949f\u8363\u5149"
+  }, 
+  {
+    "label": "\u683c\u5170\u5802"
+  }, 
+  {
+    "label": "\u614e\u601d"
+  }, 
+  {
+    "label": "\u91d1\u878d\u5b66"
+  }, 
+  {
+    "label": "\u94c1\u725b"
+  }, 
+  {
+    "label": "\u674e\u5e94\u6797"
+  }, 
+  {
+    "label": "\u60fa\u4ead"
+  }, 
+  {
+    "label": "\u660e\u8fa8"
+  }, 
+  {
+    "label": "\u4f26\u7406\u5b66"
+  }, 
+  {
+    "label": "\u72ee\u5b50"
+  }, 
+  {
+    "label": "\u9648\u5bc5\u606a"
+  }, 
+  {
+    "label": "\u9ed1\u77f3\u5c4b"
+  }, 
+  {
+    "label": "\u7b03\u884c"
+  }, 
+  {
+    "label": "\u5b8f\u89c2\u7ecf\u6d4e"
+  }, 
+  {
+    "label": "\u9ebb\u7701\u7406\u5de5\u5b66\u9662"
+  }, 
+  {
+    "label": "\u4f0d\u6cbe\u5fb7"
+  }, 
+  {
+    "label": "\u6797\u62a4\u5802"
+  }, 
+  {
+    "label": "\u4f5c\u80b2\u82f1\u624d\uff0c\u670d\u52a1\u793e\u4f1a"
+  }, 
+  {
+    "label": "\u5546\u6cd5"
+  }, 
+  {
+    "label": "\u6728\u68c9\u82b1"
+  }, 
+  {
+    "label": "\u4f0d\u821c\u5fb7"
+  }, 
+  {
+    "label": "\u5317\u95e8\u724c\u574a"
+  }, 
+  {
+    "label": "\u5e73\u539f\u5e7f\u9614\uff0c\u8fbd\u8fd1\u76ee\u524d"
+  }, 
+  {
+    "label": "\u7ec4\u7ec7\u884c\u4e3a"
+  }, 
+  {
+    "label": "\u300a\u4e2d\u5eb8\u300b"
+  }, 
+  {
+    "label": "\u9648\u5e8f\u7ecf"
+  }, 
+  {
+    "label": "\u5cad\u5357\u5802"
+  }, 
+  {
+    "label": "\u767d\u4e91\u5c71\u9ad8\uff0c\u73e0\u6c5f\u6c34\u957f"
+  }, 
+  {
+    "label": "MBA"
+  }, 
+  {
+    "label": "\u300a\u5c71\u9ad8\u6c34\u957f\u300b"
+  }, 
+  {
+    "label": "\u738b\u5c4f\u5c71"
+  }, 
+  {
+    "label": "\u82f1\u4e1c\u4f53\u80b2\u9986"
+  }, 
+  {
+    "label": "\u543e\u6821\u77d7\u7acb\uff0c\u851a\u4e3a\u56fd\u5149"
+  }, 
+  {
+    "label": "ORI"
+  }, 
+  {
+    "label": "\u6559\u7ec3"
+  }, 
+  {
+    "label": "\u53f6\u8446\u5b9a"
+  }, 
+  {
+    "label": "\u4f0d\u821c\u5fb7\u56fe\u4e66\u9986"
+  }, 
+  {
+    "label": "\u632f\u5174\u4e2d\u534e\uff0c\u6c38\u5fd7\u52ff\u5fd8"
+  }, 
+  {
+    "label": "\u5fae\u89c2\u7ecf\u6d4e"
+  }, 
+  {
+    "label": "\u5e08\u59d0"
+  }, 
+  {
+    "label": "\u8212\u5143"
+  }, 
+  {
+    "label": "\u53f6\u8446\u5b9a\u5802"
+  }, 
+  {
+    "label": "\u5fb7\u624d\u517c\u5907"
+  }, 
+  {
+    "label": "PPT"
+  }, 
+  {
+    "label": "\u5e08\u5144"
+  }, 
+  {
+    "label": "\u90b9\u81f3\u5e84"
+  }, 
+  {
+    "label": "\u8363\u5149\u5802"
+  }, 
+  {
+    "label": "\u9886\u8896\u6c14\u8d28"
+  }, 
+  {
+    "label": "python"
+  }, 
+  {
+    "label": "\u53cc\u9e2d\u5c71"
+  }, 
+  {
+    "label": "\u9c81\u8fc5"
+  }, 
+  {
+    "label": "\u4e2d\u5927\u5357\u95e8"
+  }, 
+  {
+    "label": "\u5bb6\u56fd\u60c5\u6000"
+  }, 
+  {
+    "label": "excel"
+  }, 
+  {
+    "label": "\u56fd\u7acb\u5e7f\u4e1c\u5927\u5b66"
+  }, 
+  {
+    "label": "\u5b59\u79d1"
+  }, 
+  {
+    "label": "\u8fdb\u58eb\u724c\u574a"
+  }, 
+  {
+    "label": "\u683c\u81f4"
+  }, 
+  {
+    "label": "\u671f\u6743"
+  }, 
+  {
+    "label": "\u5199\u4f5c\u4e1a"
+  }, 
+  {
+    "label": "\u9646\u519b\uff08\u9662\u957f\uff09"
+  }, 
+  {
+    "label": "\u516b\u89d2\u4ead"
+  }, 
+  {
+    "label": "\u8bda\u6b63"
+  }, 
+  {
+    "label": "\u5546\u4e1a\u6a21\u5f0f"
+  }, 
+  {
+    "label": "\u8003\u8bd5"
+  }, 
+  {
+    "label": "\u90ac\u91d1\u6d9b"
+  }, 
+  {
+    "label": "\u54f2\u751f\u5802"
+  }, 
+  {
+    "label": "\u4fee\u9f50"
+  }, 
+  {
+    "label": "\u4eba\u529b\u8d44\u6e90"
+  }, 
+  {
+    "label": "\u63d0\u9762"
+  }, 
+  {
+    "label": "\u674e\u5fd7\u660e"
+  }, 
+  {
+    "label": "\u9ebb\u91d1\u58a8\u5c4b"
+  }, 
+  {
+    "label": "\u6cbb\u5e73"
+  }, 
+  {
+    "label": "\u9886\u5bfc\u529b"
+  }, 
+  {
+    "label": "\u68a6\u60f3"
+  }, 
+  {
+    "label": "\u5eb7\u6709\u4e3a"
+  }, 
+  {
+    "label": "\u5341\u53cb\u5802"
+  }, 
+  {
+    "label": "\u7ea2\u7070\u7cbe\u795e"
+  }, 
+  {
+    "label": "\u6c9f\u901a"
+  }, 
+  {
+    "label": "985"
+  }, 
+  {
+    "label": "\u4ea8\u5229\u00b7\u9a6c\u4e01"
+  }, 
+  {
+    "label": "\u722a\u54c7\u5802"
+  }, 
+  {
+    "label": "\u56fd\u6cf0\u6c11\u5b89 \u98ce\u8c03\u96e8\u987a"
+  }, 
+  {
+    "label": "\u6536\u8d2d"
+  }, 
+  {
+    "label": "\u6821\u56ed\u8ba4\u77e5"
+  }, 
+  {
+    "label": "\u53f6\u5e86\u661f"
+  }, 
+  {
+    "label": "\u5f20\u5f3c\u58eb\u5802"
+  }, 
+  {
+    "label": "\u6bcd\u6821\u5c79\u7acb \u98ce\u6ce2\u4e0d\u6447"
+  }, 
+  {
+    "label": "\u8d22\u52a1\u62a5\u8868"
+  }, 
+  {
+    "label": "\u590d\u8bd5"
+  }, 
+  {
+    "label": "\u9648\u70b3\u8f89"
+  }, 
+  {
+    "label": "\u677e\u6d9b\u56ed"
+  }, 
+  {
+    "label": "\u5168\u7403\u89c6\u91ce\uff0c\u672a\u6765\u9886\u8896"
+  }, 
+  {
+    "label": "\u671f\u8d27"
+  }, 
+  {
+    "label": "\u5b66\u751f\u8bc1"
+  }, 
+  {
+    "label": "\u4f0d\u6dd1\u6e05"
+  }, 
+  {
+    "label": "\u5357\u8349\u576a"
+  }, 
+  {
+    "label": "\u56fd\u5185\u9886\u5148\uff0c\u56fd\u9645\u4e00\u6d41"
+  }, 
+  {
+    "label": "\u5546\u4e1a\u4fdd\u9669"
+  }, 
+  {
+    "label": "\u6821\u53cb"
+  }, 
+  {
+    "label": "\u5b8b\u5e86\u9f84"
+  }, 
+  {
+    "label": "\u5eb7\u4e50\u56ed"
+  }, 
+  {
+    "label": "\u5cad\u5357\u4eba"
+  }, 
+  {
+    "label": "\u8d44\u672c\u5e02\u573a"
+  }, 
+  {
+    "label": "\u5956\u5b66\u91d1"
+  }, 
+  {
+    "label": "\u9648\u5609\u5e9a"
+  }, 
+  {
+    "label": "\u5584\u601d\u5802"
+  }, 
+  {
+    "label": "\u4e2d\u5927\u4eba"
+  }, 
+  {
+    "label": "\u793e\u4f1a\u8d23\u4efb"
+  }, 
+  {
+    "label": "\u540d\u8a89\u535a\u58eb"
+  }, 
+  {
+    "label": "\u53f8\u5f92\u536b"
+  }, 
+  {
+    "label": "MBA\u5927\u697c"
+  }, 
+  {
+    "label": "\u56e2\u961f\u7cbe\u795e"
+  }, 
+  {
+    "label": "\u8d44\u4ea7\u914d\u7f6e"
+  }, 
+  {
+    "label": "\u5bfc\u5e08"
+  }, 
+  {
+    "label": "\u6881\u542f\u8d85"
+  }, 
+  {
+    "label": "\u5927\u949f\u697c"
+  }, 
+  {
+    "label": "\u5b66\u4ee5\u81f4\u7528\uff0c\u4e0d\u60dc\u81ea\u5f3a"
+  }, 
+  {
+    "label": "\u4f1a\u8ba1"
+  }, 
+  {
+    "label": "\u9009\u8bfe"
+  }, 
+  {
+    "label": "\u9a6c\u5e94\u5f6a"
+  }, 
+  {
+    "label": "\u8d85\u6cc9"
+  }, 
+  {
+    "label": "\u8981\u505a\u5927\u4e8b\uff0c\u4e0d\u53ef\u505a\u5927\u5b98"
+  }, 
+  {
+    "label": "\u4ea7\u4e1a\u5347\u7ea7"
+  }, 
+  {
+    "label": "\u6838\u5fc3\u8bfe"
+  }, 
+  {
+    "label": "\u9ec4\u534e"
+  }, 
+  {
+    "label": "\u6881\u92b6\u741a\u5802"
+  }, 
+  {
+    "label": "\u6c11\u4e3b"
+  }, 
+  {
+    "label": "\u98ce\u9669\u6295\u8d44"
+  }, 
+  {
+    "label": "\u72b6\u5143"
+  }, 
+  {
+    "label": "\u51bc\u661f\u6d77"
+  }, 
+  {
+    "label": "\u9ec4\u4f20\u7ecf\u5802"
+  }, 
+  {
+    "label": "\u52a1\u5b9e"
+  }, 
+  {
+    "label": "\u98ce\u9669\u7ba1\u7406"
+  }, 
+  {
+    "label": "\u5f55\u53d6"
+  }, 
+  {
+    "label": "\u5f20\u5f3c\u58eb"
+  }, 
+  {
+    "label": "\u6625\u6656\u56ed"
+  }, 
+  {
+    "label": "\u7231\u56fd"
+  }, 
+  {
+    "label": "\u516c\u53f8\u6cbb\u7406"
+  }, 
+  {
+    "label": "\u5f00\u5b66"
+  }, 
+  {
+    "label": "\u4f0a\u838e\u8d1d\u00b7\u9ed1\u77f3"
+  }, 
+  {
+    "label": "\u5927\u5b66\u57ce"
+  }, 
+  {
+    "label": "\u7231\u6821"
+  }, 
+  {
+    "label": "\u8d44\u672c\u8fd0\u4f5c"
+  }, 
+  {
+    "label": "\u6691\u5047"
+  }, 
+  {
+    "label": "\u6881\u7fbd\u751f"
+  }, 
+  {
+    "label": "\u4e2d\u5927\u5317\u95e8"
+  }, 
+  {
+    "label": "\u4e00\u65e5\u5cad\u5357\u4eba\uff0c\u4e00\u751f\u5cad\u5357\u4eba"
+  }, 
+  {
+    "label": "\u5e76\u8d2d\u91cd\u7ec4"
+  }, 
+  {
+    "label": "1988\u5e74"
+  }, 
+  {
+    "label": "SUN YAT-SEN"
+  }, 
+  {
+    "label": "\u65b0\u6e2f\u897f\u8def"
+  }, 
+  {
+    "label": "\u4e2d\u5c71\u624b\u521b\uff0c\u9057\u6cfd\u4f59\u82b3"
+  }, 
+  {
+    "label": "\u80a1\u6743"
+  }, 
+  {
+    "label": "1888\u5e74"
+  }, 
+  {
+    "label": "\u90ed\u6cab\u82e5"
+  }, 
+  {
+    "label": "\u5e7f\u5dde"
+  }, 
+  {
+    "label": "\u6fc0\u52b1\u673a\u5236"
+  }, 
+  {
+    "label": "1924\u5e74"
+  }, 
+  {
+    "label": "\u80af\u5c3c\u8fea\u592b\u4eba"
+  }, 
+  {
+    "label": "\u73e0\u6d77"
+  }, 
+  {
+    "label": "\u6218\u7565"
+  }, 
+  {
+    "label": "\u4e2d\u5c71\u533b"
+  }, 
+  {
+    "label": "\u9648\u8f91\u4e94"
+  }, 
+  {
+    "label": "\u6df1\u5733"
+  }, 
+  {
+    "label": "\u6295\u8d44"
+  }, 
+  {
+    "label": "\u8bba\u6587"
+  }, 
+  {
+    "label": "\u9ebb\u91d1\u58a8\u592b\u4eba"
+  }, 
+  {
+    "label": "\u73e0\u6c5f\u7801\u5934"
+  }, 
+  {
+    "label": "\u8fd0\u8425"
+  }, 
+  {
+    "label": "\u4eba\u624d"
+  }, 
+  {
+    "label": "\u683c\u81f4\u4e66\u9662"
+  }, 
+  {
+    "label": "IPO"
+  }, 
+  {
+    "label": "\u7ba1\u7406\u7c7b\u8054\u8003"
+  }, 
+  {
+    "label": "\u5e7f\u5bd2\u5bab"
+  }, 
+  {
+    "label": "EMBA"
+  }, 
+  {
+    "label": "\u6559\u6388"
+  }, 
+  {
+    "label": "\u9646\u4f51\u5802"
+  }, 
+  {
+    "label": "\u517c\u5e76"
+  }, 
+  {
+    "label": "\u751f\u6001\u5b66"
+  }, 
+  {
+    "label": "\u5b66\u4eba\u7ba1"
+  }, 
+  {
+    "label": "\u5b9a\u4ef7"
+  }, 
+  {
+    "label": "\u53cc\u4e00\u6d41"
+  }, 
+  {
+    "label": "\u4e2d\u6587\u5802"
+  }, 
+  {
+    "label": "\u5546\u4e1a\u5206\u6790"
+  }, 
+  {
+    "label": "\u5546\u5b66\u9662"
+  }, 
+  {
+    "label": "\u9038\u592b\u697c"
+  }, 
+  {
+    "label": "\u54f2\u5b66"
+  }
+]
